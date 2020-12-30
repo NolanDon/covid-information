b6e3f2e32cb7ab66ea984fd1d8e02e3324b82cf7
Nolan <nolan.donb@gmail.com>
2020-12-29 21:50:17 -0700

    Initial Commit

---
+++ Covid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Covid
+//
+//  Created by Nolan on 2020-12-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
